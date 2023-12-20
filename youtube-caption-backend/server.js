af264d042a282ab3d5c9a862c1b83fc3025656e7
Ali Shan <optimist.alishan@gmail.com>
2023-12-21 04:15:23 +0500

    pushing new amazing changes

--- youtube-caption-backend/server.js
+++ youtube-caption-backend/server.js
@@ -3,44 +3,77 @@ const axios = require('axios');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
 const https = require('https');
+const xml2js = require('xml2js');
 
 const app = express();
 const PORT = 3001;
+let lang = 'en'; // Global language variable
 
 const cors = require('cors');
-// Allow all origins
-app.use(cors());
-// Allow specific origin(s)
-app.use(cors({
-  origin: 'https://yt-cap-download.vercel.app/'
-}));
-
 app.use(cors());
 app.use(express.json());
 
 app.post('/download-caption', async (req, res) => {
   const { videoId } = req.body;
-  const lang = 'en';
   const format = 'xml';
+  const clientLang = req.query.lang; // Get language from client query parameter
 
   try {
     const info = await ytdl.getInfo(videoId);
     const tracks = info.player_response.captions.playerCaptionsTracklistRenderer.captionTracks;
 
     if (tracks && tracks.length) {
-      const track = tracks.find(t => t.languageCode === lang);
+      const track = tracks.find(t => t.languageCode === (clientLang || lang)); // Use clientLang if available, otherwise use the default lang
 
       if (track) {
-        const output = `${info.videoDetails.title}.${track.languageCode}.${format}`;
         const captionLink = `${track.baseUrl}&fmt=${format !== 'xml' ? format : ''}`;
 
-        https.get(captionLink, (response) => {
-          response.pipe(fs.createWriteStream(path.resolve(__dirname, output)));
-        });
+        https.get(captionLink, async (response) => {
+          let xmlData = '';
 
-        res.json({ captionLink });
+          response.on('data', (chunk) => {
+            xmlData += chunk;
+          });
+
+          response.on('end', async () => {
+            const parser = new xml2js.Parser();
+            parser.parseString(xmlData, async (err, result) => {
+              if (err) {
+                console.error('Error parsing XML:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+              } else {
+                const textContent = result.transcript.text.map(textElement => textElement._);
+                const processedText = textContent.join(' ');
+
+                const outputFileName = `${info.videoDetails.title}_${lang}.txt`;
+                const outputPath = path.resolve(__dirname, outputFileName);
+
+                fs.writeFile(outputPath, processedText, (writeErr) => {
+                  if (writeErr) {
+                    console.error('Error writing to file:', writeErr);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                  } else {
+                    // Set Content-Disposition header to trigger download
+                    res.download(outputPath, outputFileName, (downloadErr) => {
+                      if (downloadErr) {
+                        console.error('Error downloading file:', downloadErr);
+                        res.status(500).json({ error: 'Internal Server Error' });
+                      } else {
+                        // Delete the temporary file after it has been sent
+                        fs.unlink(outputPath, (unlinkErr) => {
+                          if (unlinkErr) {
+                            console.error('Error deleting file:', unlinkErr);
+                          }
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          });
+        });
       } else {
         res.status(404).json({ error: 'Could not find captions for the specified language' });
       }
@@ -49,6 +82,24 @@ app.post('/download-caption', async (req, res) => {
     }
   } catch (error) {
     console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/video-details', async (req, res) => {
+  const { videoId } = req.query;
+
+  try {
+    const info = await ytdl.getInfo(videoId);
+    const title = info.videoDetails.title;
+    
+    // Get the highest resolution thumbnail
+    const thumbnails = info.videoDetails.thumbnail.thumbnails;
+    const thumbnailUrl = thumbnails.reduce((prev, curr) => (prev.width > curr.width ? prev : curr)).url;
+
+    res.json({ title, thumbnail: thumbnailUrl });
+  } catch (error) {
+    console.error('Error fetching video details:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
