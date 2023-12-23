446eab4f42ff3d5aae2b7bc9b58c4ddcb09ecb99
Ali Shan <optimist.alishan@gmail.com>
2023-12-24 04:46:59 +0500

    pushing

--- youtube-caption-backend/server.js
+++ youtube-caption-backend/server.js
@@ -10,7 +10,7 @@ const cheerio = require('cheerio');
 
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 let lang = 'en';
 
 const cors = require('cors');
--- youtube-caption-downloader/src/components/home.jsx
+++ youtube-caption-downloader/src/components/home.jsx
@@ -33,7 +33,7 @@ function Home() {
   const handleDownloadClick = async (videoId) => {
     try {
       const response = await axios.post(
-        `https://yt-fullstack.vercel.app/:3001/download-caption?lang=${selectedLanguage}`,
+        `https://yt-fullstack.vercel.app/download-caption?lang=${selectedLanguage}`,
         { videoId },
         { responseType: "arraybuffer" }
       );
@@ -60,7 +60,7 @@ function Home() {
 
   const handleSearch = async () => {
     try {
-      const response = await axios.post("https://yt-fullstack.vercel.app/:3001/search-videos", {
+      const response = await axios.post("https://yt-fullstack.vercel.app/search-videos", {
         query,
       });
       const sortedVideos = response.data.videos.sort(
